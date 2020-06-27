966be26cd53045254434a5e31e725a3273a90824
JAWWAD AKHTER <55801541+jawwadakhter674@users.noreply.github.com>
2020-06-27 12:32:39 +0500

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,21 @@
+
+
+function getNum(num){
+    
+    var result=document.getElementById("result");
+result.value +=num;
+}
+
+
+
+function getClear(){
+    var result=document.getElementById("result");
+    result.value =0;
+}
+
+
+function getResult(){
+    var result=document.getElementById("result");
+    result.value=eval(result.value)
+}
+
